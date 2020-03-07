90df6ec5297e1aca9d9e09a5eec91eb9940d7a31
Panchenko Tetiana <35835232+einoorish@users.noreply.github.com>
2020-03-07 17:06:35 +0200

    fixed PriorityQueue

--- 2nd term/Lab2_b/PriorityQueue.h
+++ 2nd term/Lab2_b/PriorityQueue.h
@@ -22,8 +22,51 @@ class PriorityQueue {
 
     public:
         bool isEmpty() const {return queueSize == 0;}
-        void enqueue(T data, int priority);
-        T dequeue() noexcept(false);
+        void enqueue(T data, int priority){
+            Node* node = new Node(data, priority);
+            queueSize++;
+
+            if(front == nullptr){// or length == 0
+                front = rear = node;
+                return;
+            }
+
+            //if new node's priority is higher than the front's...
+            if (front->priority > priority || (front->priority == priority && front->data > data)) {
+                // insert it as first (before head)
+                node->next = front;
+                front = node;
+            }
+            //if new node's priority is lower than the rear's...
+            else if (rear->priority < priority|| (rear->priority == priority && rear->data < data)){
+                // insert it as last (after rear)
+                rear->next = node;
+                rear = node;
+            }
+            //else it should be inserted somewhere in the middle
+            else {
+                Node* current = rear, *prev;
+                while(current!= nullptr &&
+                      current->priority < priority && current->data < data){
+                    prev = current;
+                    current = current->next;
+                }
+                node->next = current;
+                prev->next = node;
+            }
+
+        }
+        T dequeue() noexcept(false){
+            if(isEmpty()) throw std::invalid_argument("Queue is empty\n");
+            Node* temp = front;
+            T data = temp->data;
+            front = temp->next;
+
+            if (front == nullptr) rear = nullptr;
+            free(temp);
+            queueSize--;
+            return data;
+        }
         friend std::ostream& operator<<(std::ostream& os, const PriorityQueue& stack){
             Node* current = stack.front;
             while(current != nullptr){
--- 2nd term/Lab2_b/main.cpp
+++ 2nd term/Lab2_b/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include "PriorityQueue.h"
 
-int main() {
+void priorityQueue_test(){
     PriorityQueue<int> pq = PriorityQueue<int>();
+    try{
+        pq.dequeue();
+    } catch (std::exception& e ){
+        std::cout<< e.what();
+    }
     pq.enqueue(4,1);
     pq.enqueue(5,2);
     pq.enqueue(6,3);
     pq.enqueue(7,0);
 
     pq.dequeue();
+    pq.enqueue(2,1);
     std::cout<<pq.dequeue()<<std::endl;
 
     std::cout << pq;
+}
+
+int main() {
+
+        priorityQueue_test();
 
     return 0;
 }
