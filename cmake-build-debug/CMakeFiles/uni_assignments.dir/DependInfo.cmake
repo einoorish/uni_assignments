041d9ad48a6ff84d32525145c0a8026dacf394b3
rllst <serega.goroshkevych@gmail.com>
2021-05-25 22:27:57 +0300

    cmake update

---
+++ cmake-build-debug/CMakeFiles/uni_assignments.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/User/Desktop/uni_assignments/4th term/OOP/Lab2/factory.cpp" "C:/Users/User/Desktop/uni_assignments/cmake-build-debug/CMakeFiles/uni_assignments.dir/4th_term/OOP/Lab2/factory.cpp.obj"
+  "C:/Users/User/Desktop/uni_assignments/4th term/OOP/Lab2/main.cpp" "C:/Users/User/Desktop/uni_assignments/cmake-build-debug/CMakeFiles/uni_assignments.dir/4th_term/OOP/Lab2/main.cpp.obj"
+  "C:/Users/User/Desktop/uni_assignments/4th term/OOP/Lab2/mainwindow.cpp" "C:/Users/User/Desktop/uni_assignments/cmake-build-debug/CMakeFiles/uni_assignments.dir/4th_term/OOP/Lab2/mainwindow.cpp.obj"
+  "C:/Users/User/Desktop/uni_assignments/4th term/OOP/Lab2/solve.cpp" "C:/Users/User/Desktop/uni_assignments/cmake-build-debug/CMakeFiles/uni_assignments.dir/4th_term/OOP/Lab2/solve.cpp.obj"
+  "C:/Users/User/Desktop/uni_assignments/4th term/OOP/Lab2/strategy.cpp" "C:/Users/User/Desktop/uni_assignments/cmake-build-debug/CMakeFiles/uni_assignments.dir/4th_term/OOP/Lab2/strategy.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../4th term/OOP/Lab2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
