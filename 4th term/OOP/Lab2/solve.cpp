77a94450eb48d44dd3e9d7b316247db97870265d
rllst <serega.goroshkevych@gmail.com>
2021-06-15 01:00:22 +0300

    jacoi added

--- 4th term/OOP/Lab2/solve.cpp
+++ 4th term/OOP/Lab2/solve.cpp
@@ -1,5 +1,5 @@
 #include "solve.h"
-
+#include <cmath>
 double* Solve::startMethod (Matrix& matrix){
     this->_matrix = &matrix;
     double* result = solvingMethod();
@@ -29,15 +29,33 @@ double* Jacobi::solvingMethod() {
     if (this->_matrix->determinant() == 0)
         return nullptr;
     auto *ans = new double[_matrix->getCols()];
-
-
-
+    for(int i=0;i<_matrix->getCols();i++) ans[i]=1;
+    double eps=0.01;
+    double* temp = new double[_matrix->getCols()];
+    double norm;
+    do {
+        for (int i = 0; i < _matrix->getCols(); i++) {
+            temp[i] = _matrix->_free_var[i];
+            for (int g = 0; g < _matrix->getCols(); g++) {
+                if (i != g)
+                    temp[i] -= _matrix->_data[i][g] * ans[g];
+            }
+            temp[i] /= _matrix->_data[i][i];
+        }
+        norm = fabs(ans[0] - temp[0]);
+        for (int h = 0; h < _matrix->getCols(); h++) {
+            if (fabs(ans[h] - temp[h]) > norm)
+                norm = fabs(ans[h] - temp[h]);
+            ans[h] = temp[h];
+        }
+    } while (norm > eps);
     return ans;
 }
 double* Zeidel::solvingMethod() {
     if (this->_matrix->determinant() == 0)
         return nullptr;
     auto *ans = new double[_matrix->getCols()];
+for(int i=0;i<_matrix->getCols();i++) ans[i]=1;
 
 
 
